f90f1d2e6e44f799da2dd1cdb2b792d1c2b41fb9
ndnam198 <ndnam198@gmail.com>
2020-10-25 14:52:10 +0700

    [10/25/20 14:52:10]:

--- commit.sh
+++ commit.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+timestamp=$(date +"%D %T")
+message=$1
 git add -A;
-git commit -m "$1";
+git commit -m "[$timestamp]: $message"
 git push https://github.com/ndnam198/STM32F1-UART_RX_DMA.git UART_CLI
